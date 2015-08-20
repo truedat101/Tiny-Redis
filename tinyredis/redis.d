5b5104b96deb8fc59973a528c0e37f5572f9b66f
David J. Kordsmeier <dkords@gmail.com>
2015-08-19 20:37:54 -0700

    Implement a proper shutdown, fix the test cases to not get stuck, remove debug

--- tinyredis/redis.d
+++ tinyredis/redis.d
@@ -6,7 +6,7 @@ module tinyredis.redis;
 
 private:
     import std.array : appender;
-    import std.socket : TcpSocket, InternetAddress;
+    import std.socket : TcpSocket, InternetAddress, SocketShutdown;
     import std.traits;
     import core.thread;
     import core.atomic;
@@ -30,7 +30,9 @@ public :
             TcpSocket conn;
             CallbackFunc[string] subscribers; // Key by channel
             Thread subworker;
-            shared(bool) isSubworkerDone; 
+            shared(bool) isSubworkerDone;
+            bool isConnValid;
+
             void f1() { // XXX Maybe extend Thread instead
                 auto keys = subscribers.keys();
                 auto channels = "";
@@ -55,7 +57,7 @@ public :
                     Response[] r = receiveResponses(conn, 1);
 
                     for (int j = 0; j < r.length; j++) {
-                        writeln(r[j]);
+                        // writeln(r[j]);
                         writeln("Processing ", (r.length-j), " more responses");
                         subscriberContext context = new subscriberContext();
                         context.totalremaining = cast(int) r.length-j-1;
@@ -69,6 +71,7 @@ public :
                 // Unsubscribe you jerk
                 //
                 cmd = "UNSUBSCRIBE " ~ channels;
+                writeln("unsubsribe all");
                 conn.send(toMultiBulk(cmd));
                 // XXX Do we really need to check the response?
                 writeln("subworker is done, returning");
@@ -82,8 +85,21 @@ public :
         this(string host = "127.0.0.1", ushort port = 6379)
         {
             conn = new TcpSocket(new InternetAddress(host, port));
+            isConnValid = true;
         }
         
+        bool isRedisConnValid() {
+            return isConnValid;
+        }
+
+        void shutdown() {
+            if (conn !is null) {
+                conn.shutdown(SocketShutdown.BOTH);
+                conn.close();
+                isConnValid = false;
+                conn = null;
+            }
+        }
         /**
          * Call Redis using any type T that can be converted to a string
          *
@@ -152,6 +168,8 @@ public :
 
         /*
              Insert the subscriber for the channel, and replace it if it exists already
+
+             XXX TODO: Implement multichannel signature ... also, multiple channels with a single cb
         */
         void subscribe(string channel, CallbackFunc cb) {
             subscribers[channel] = cb;
@@ -198,7 +216,7 @@ public :
 
             //
             // XXX Need a better way to be sure the subscription thread is done and not stuck
-            //
+            // 
             if (subworker is null) {
                 writeln("subworker is null");
                 return status;
@@ -312,6 +330,8 @@ unittest
 {
     auto redis = new Redis();
     auto redis2 = new Redis();
+
+    assert(redis.isRedisConnValid() == true);
     auto response = redis.send("LASTSAVE");
     assert(response.type == ResponseType.Integer);
     
@@ -424,22 +444,31 @@ unittest
         writeln("cb1 invoked with resp:", resp.toDiagnosticString(), " context.totalremaining=", (cast(subscriberContext) context).totalremaining, " context.channel=", (cast(subscriberContext) context).channel);
     };
 
+    CallbackFunc cb2 = function(context, resp) {
+        writeln("cb2 invoked with resp:", resp.toDiagnosticString(), " context.totalremaining=", (cast(subscriberContext) context).totalremaining, " context.channel=", (cast(subscriberContext) context).channel);
+    };
+
     //
-    // Test the blocking call for sub ... TODO, add this to a thread
-    //
+    // Test the blocking call for sub ... XXX TODO, add this to a thread
+    // 
     /*
     redis.subBlock("foochan foochan2", cb1);
     */
     
 
     redis.subscribe("foochan", cb1);
+    redis.subscribe("baaasim", cb2);
+    assert(redis.getSubscriberCount() == 2);
+
+    redis.unsubscribe("baaasim");
+    redis.unsubscribe("babasim");
     assert(redis.getSubscriberCount() == 1);
     assert(redis.startSubscriptions() == 0);
     assert(redis.startSubscriptions() == -1);
     
-    for (int i = 0; i < 100000; i++) {
+    /* for (int i = 0; i < 100000; i++) {
         float x = 10*10.0*10;
-    }
+    } */
 
     if (redis.areSubscriptionsStarted()) {
         //
@@ -447,11 +476,11 @@ unittest
         // #2 style
         //
         redis2.send("PUBLISH foochan again1");
-        redis2.send("PUBLISH foochan againagain2");
+        redis2.send("PUBLISH foochan2 againagain2");
         redis2.send("PUBLISH foochan againagainagain3");
         redis2.send("PUBLISH foochan again4");
         redis2.send("PUBLISH foochan againagain5");
-        redis2.send("PUBLISH foochan againagainagain6");
+        redis2.send("PUBLISH foochan2 againagainagain6");
         redis2.send("PUBLISH foochan again7");
         redis2.send("PUBLISH foochan againagain8");
         redis2.send("PUBLISH foochan againagainagain9");
@@ -461,7 +490,7 @@ unittest
         redis2.send("PUBLISH foochan again13");
         redis2.send("PUBLISH foochan againagain14");
         redis2.send("PUBLISH foochan againagainagain15");
-        redis2.send("PUBLISH foochan again16");
+        redis2.send("PUBLISH foochan2 again16");
         redis2.send("PUBLISH foochan againagain17");
         redis2.send("PUBLISH foochan againagainagain18");
         redis2.send("PUBLISH foochan again19");
@@ -472,7 +501,7 @@ unittest
         redis2.send("PUBLISH foochan againagainagain24");
         redis2.send("PUBLISH foochan again25");
         redis2.send("PUBLISH foochan againagain26");
-        redis2.send("PUBLISH foochan againagainagain27");
+        redis2.send("PUBLISH foochan2 againagainagain27");
     }
     Thread.sleep(5.seconds);
 
@@ -480,6 +509,10 @@ unittest
 
     assert(redis.stopSubscriptions() == 0);
     assert(redis.stopSubscriptions() == -1);
+
+    redis.shutdown();
+    assert(redis.isRedisConnValid() == false);
+    redis2.shutdown();
     writeln("DONE WITH TESTS");
     //
     // XXX NOTE, thread is stuck waiting here, fix this ... subscriber should release thread
